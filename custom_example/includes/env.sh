41596b6a3d4672d5e79fe66198e6748c06af7f8f
Halil Özgür <226712+halilim@users.noreply.github.com>
2025-01-08 14:36:45 +0300

    Improve Nginx config, etc.

--- custom_example/includes/env.sh
+++ custom_example/includes/env.sh
@@ -1,13 +1,14 @@
 alias envshc='$EDITOR "$DOTFILES_CUSTOM"/includes/env.sh' # cSpell:ignore envshc
 
 export ADMINER_DIR="$HOME/code/tools/adminer"
+export CODE_ROOT=~/code
 export ESHEAD_DIR="$HOME/code/tools/elasticsearch-head"
+export FOO_PORT=8087 # Custom local app, start its dev server with this port too
 export GPG_KEY=123ABC # gpg --list-keys > The hex number below "pub"
 # Replace ld_client with your LaunchDarkly client, or the whole command
 export LAUNCH_DARKLY_KEYS_CMD=(bin/rails runner 'puts ld_client.all_features.keys')
 # %s will be replaced with the flag key
 export LAUNCH_DARKLY_URL='https://app.launchdarkly.com/projects/default/flags/%s/targeting?env=production&env=development&selected-env=development'
-export PHP_ROOT=~/code
 export RUBY_CMD_PREFIX='bin/'
 
 DOTFILES_INCLUDE_LIBS+=(example_lib gpg rbenv)
