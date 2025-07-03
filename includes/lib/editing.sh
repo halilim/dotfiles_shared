fd28d4fef3ea04958222fab6221d4bfd7620c79d
Halil Özgür <226712+halilim@users.noreply.github.com>
2025-07-03 18:43:14 +0300

    Fix paths with spaces in vim_open

--- includes/lib/editing.sh
+++ includes/lib/editing.sh
@@ -24,25 +24,28 @@ function open_with_editor() {
 }
 
 function vim_open() {
-  local vim_cmd_=''
+  local arg_arr=("$@") vim_cmd_=''
 
   if [[ ${SUDO:-} ]]; then
-    vim_cmd_+='sudo'
+    vim_cmd_+='sudo '
   fi
 
-  vim_cmd_+=" $VIM_CMD"
+  vim_cmd_+="$VIM_CMD"
+
+  local arg_ct=${#arg_arr[@]} pct_qs
+  pct_qs=$(printf ' %%q%.0s' $(seq 1 "$arg_ct"))
 
   # https://stackoverflow.com/a/5945322/372654
   if [[ "$#" -gt 0 ]]; then
     if [[ -d $1 ]]; then
-      vim_cmd_+=" $* +':lcd %%'"
+      vim_cmd_+="$pct_qs +':lcd %%'"
     else
       if [[ ! ${VIM_NO_SERVER:-} ]]; then
         vim_cmd_+=" --remote-silent"
       fi
-      vim_cmd_+=" $*"
+      vim_cmd_+="$pct_qs"
     fi
   fi
 
-  echo_eval "$vim_cmd_"
+  echo_eval "$vim_cmd_" "${arg_arr[@]}"
 }
--- spec/includes/lib/editing_spec.sh
+++ spec/includes/lib/editing_spec.sh
@@ -2,21 +2,45 @@ Include includes/lib/editing.sh
 Include includes/lib/functions.sh # For echo_eval
 
 Describe 'open_with_editor'
-  function code() {
-    if [[ $1 != '-g' || $2 != 'foo' || $3 != 'bar baz' ]]; then
-      echo >&2 'Invalid code call'
-      return 1
-    fi
+  Context 'when the editor is VS Code'
+    export EDITOR=code
 
-    return 0
-  }
+    function code() {
+      if [[ $1 != '-g' || $2 != 'foo' || $3 != 'bar baz' ]]; then
+        echo >&2 'Invalid code call'
+        return 1
+      fi
 
-  export EDITOR=code
+      return 0
+    }
 
-  It 'opens given paths with editor'
-    When call open_with_editor foo 'bar baz'
-    The stdout should eq ''
-    The stderr should eq '-> code -g foo bar\ baz'
-    The status should eq 0
+    It 'opens given paths with editor'
+      When call open_with_editor foo 'bar baz'
+      The stdout should eq ''
+      The stderr should eq '-> code -g foo bar\ baz'
+      The status should eq 0
+    End
+  End
+
+  Context 'when the editor is Vim'
+    export EDITOR=/foo/bar/bin/vim
+    export VIM_CMD=vim
+
+    function vim() {
+      if [[ $1 != '--remote-silent' || $2 != 'foo' || $3 != 'bar baz' ]]; then
+        echo >&2 'Invalid vim call'
+        return 1
+      fi
+
+      return 0
+    }
+
+    It 'opens given paths with editor'
+      When call open_with_editor foo 'bar baz'
+      The stdout should eq ''
+      The stderr should eq '-> vim_open foo bar\ baz
+-> vim --remote-silent foo bar\ baz'
+      The status should eq 0
+    End
   End
 End
