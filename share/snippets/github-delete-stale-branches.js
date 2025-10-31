c9f7a3a448d3bd637b6a9fd8b061e2cf4683e273
Halil Özgür <226712+halilim@users.noreply.github.com>
2025-10-31 08:37:45 +0300

    Backup updates

--- share/snippets/github-delete-stale-branches.js
+++ share/snippets/github-delete-stale-branches.js
@@ -2,17 +2,25 @@
 // 2. Open DevTools Console
 // 3. Paste the following code
 
-const sleep = async (/** @type {number} */ ms) => new Promise((r) => setTimeout(r, ms));
+async function sleep(/** @type {number} */ ms) {
+  return new Promise((r) => setTimeout(r, ms));
+}
 
 // To stop it anytime, set stopPageIteration = true;
 // (e.g., when it reaches a page you want to stop at, or it hits rate limits (429 Too Many Requests))
 // To resume, set stopPageIteration = false; and call iteratePages(...) again.
 let stopPageIteration = false;
-const iteratePages = async (
-  /** @type {{ (): Promise<number> | number; }} */ fn,
-  /** @type {number} */ sleepMs
-) => {
-  while (!stopPageIteration && (await fn()) > 0) {
+
+/**
+ * @param {{fn: function(any): Promise<number> | number, fnArgs?: any, continueOnEmptyPages?: boolean, sleepMs: number}} args
+ * @returns {Promise<void>}
+ */
+async function iteratePages({ fn, fnArgs = [], continueOnEmptyPages = false, sleepMs }) {
+  while (
+    !stopPageIteration &&
+    // @ts-ignore
+    ((await fn.apply(null, [fnArgs].flat())) > 0 || continueOnEmptyPages)
+  ) {
     let nextLink = document.querySelector('a[rel=next]');
     if (!nextLink) {
       break;
@@ -21,13 +29,13 @@ const iteratePages = async (
     nextLink.click();
     await sleep(sleepMs);
   }
-};
+}
 
 // 4. a) If there are only ~ a few hundred branches, you can do it in the browser:
 //       If more, this gets "429 Too Many Requests" a lot earlier; see 4. b).
 
-const deleteStaleBranchesOnPage = async () => {
-  let btns = document.querySelectorAll('td:nth-child(6) button:has(> .octicon-trash)');
+async function deleteStaleBranchesOnPage() {
+  let btns = document.querySelectorAll('td:nth-child(6) button:has(.octicon-trash)');
   for (let i = 0; i < btns.length; i++) {
     // @ts-ignore
     btns[i].click();
@@ -36,9 +44,9 @@ const deleteStaleBranchesOnPage = async () => {
     }
   }
   return btns.length;
-};
+}
 
-iteratePages(deleteStaleBranchesOnPage, 2500);
+iteratePages({ fn: deleteStaleBranchesOnPage, sleepMs: 2500 });
 
 // 4. b) If there are many branches, or if you got rate-limited in the browser, you can use the API
 //       (It seems it's not affected by the rate limits of the WEB UI, and/or it has higher limits,
@@ -49,15 +57,30 @@ iteratePages(deleteStaleBranchesOnPage, 2500);
 /** @type {string[]} */
 const branchNames = [];
 
-/** @return {number} */
-const listBranchesOnPage = () => {
-  const branches = document.querySelectorAll('td:nth-child(1)');
+/**
+ * @param {{onlyMergedOrClosedPRs?: boolean}} args
+ * @returns {number}
+ */
+function collectBranchesOnPage({ onlyMergedOrClosedPRs = false } = {}) {
+  let selector = 'td:nth-child(1)';
+  if (onlyMergedOrClosedPRs) {
+    selector = `tr:has(.octicon-git-pull-request-closed, .octicon-git-merge) ${selector}`;
+  }
+  const branches = document.querySelectorAll(selector);
   // @ts-ignore
   branchNames.push(...Array.from(branches).map((b) => b.innerText));
   return branches.length;
-};
+}
 
-iteratePages(listBranchesOnPage, 2000);
+iteratePages({ fn: collectBranchesOnPage, sleepMs: 2000 });
+
+// To collect only branches with merged or closed PRs (exclude open PRs, or branches without PRs):
+iteratePages({
+  fn: collectBranchesOnPage,
+  fnArgs: { onlyMergedOrClosedPRs: true },
+  continueOnEmptyPages: true,
+  sleepMs: 2000,
+});
 
 // 4. b) Continued: After it finishes (or stopPageIteration = true) copy the output from the console:
 console.log(branchNames); // Right click -> Copy object, save as branches.json
